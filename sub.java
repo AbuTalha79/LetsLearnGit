575e9d3c9bae1acb3ff0a1922ff42c5f8614c31c
Mohammad AbuTalha <abutalha1033@outlook.com>
2022-09-13 22:50:19 +0530

    adding diff.java

--- sub.java
+++ sub.java
@@ -1,7 +1,7 @@
 public class sub {
     public static void main(String[] args) {
         int a=10;int b=20;
-        System.out.println(a+b);
+        System.out.println("The sum is :" +(a+b));
     }
     
 }
